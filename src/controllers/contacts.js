0929978aa5cac13105e86727e58ad3a3558c76b9
EevgenK <45nalitay45@gmail.com>
2025-01-18 18:35:03 +0200

    sorting was added

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,10 +7,13 @@ import {
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactsController = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
-  const contacts = await getAllContacts(page, perPage);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+
+  const contacts = await getAllContacts({ page, perPage, sortBy, sortOrder });
   res.json({
     status: 200,
     message: 'Successfully found contacts!',
